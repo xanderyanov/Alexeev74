c484d2bd5d63e86ade1c7b1679ba73272d5803ed
xanderyanov <xanderyanov@gmail.com>
2021-10-14 10:29:15 +0400

    p4-18-17

---
+++ Domain/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Alexeev74.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alexeev74.Domain
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TextField> TextFields { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+    }
+
+
+}
---
+++ Domain/Entities/ServiceItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alexeev74.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Заполните название услуги")]
+        [Display(Name = "Название услуги")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Краткое описание услуги")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание услуги")]
+        public override string Text { get; set; }
+    }
+}
